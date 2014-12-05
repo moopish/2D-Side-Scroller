9c0660e56b83224b0778dbf4a8c947a04a39217c
Michael van Dyk <msvandyk@hotmail.com>
2014-12-05 02:06:16 -0500

    Added to the Res class, position/movement

--- src/Ludus/Res/Res.java
+++ src/Ludus/Res/Res.java
@@ -1,7 +1,25 @@
 package Ludus.Res;
 
+import Ludus.Utilitates.Situs;
+
 /**
  * Created by Michael on 04/12/2014.
+ *
+ * Res Class (Thing Class)
  */
-public class Res {
+public abstract class Res {
+
+    private Situs location;
+    private Situs movement;
+
+    protected Res(){
+
+    }
+
+    public void setLocation(Situs location)   { this.location.set(location); }
+    public void setLocation(float x, float y) { this.location.set(x, y);     }
+
+    public void setMovement(Situs movement)   { this.movement.set(movement); }
+    public void setMovement(float x, float y) { this.movement.set(x, y);     }
+
 }
