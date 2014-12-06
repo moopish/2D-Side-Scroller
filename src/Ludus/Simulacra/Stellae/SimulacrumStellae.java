ac585b27a47fcf17877671f8bb95949b5c3c790e
Michael van Dyk <msvandyk@hotmail.com>
2014-12-05 19:24:02 -0500

    Created shell for the drawing class of a star

---
+++ src/Ludus/Simulacra/Stellae/SimulacrumStellae.java
@@ -0,0 +1,20 @@
+package Ludus.Simulacra.Stellae;
+
+import Ludus.Simulacra.Simulacrum;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by Michael on 05/12/2014.
+ *
+ *  SimulacrumStellae class (image of star class)
+ *      Draw class for a star object
+ *
+ */
+public class SimulacrumStellae implements Simulacrum {
+
+    public void draw (BufferedImage image) {
+
+    }
+
+}
