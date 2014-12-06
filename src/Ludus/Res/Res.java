08e6524793d044b5e000ac117a20d96d26d9f8fc
Michael van Dyk <msvandyk@hotmail.com>
2014-12-05 19:24:34 -0500

    Added drawer attribute, constructors, getters

--- src/Ludus/Res/Res.java
+++ src/Ludus/Res/Res.java
@@ -1,7 +1,10 @@
 package Ludus.Res;
 
+import Ludus.Simulacra.Simulacrum;
 import Ludus.Utilitates.Angulus;
 import Ludus.Utilitates.Situs;
+
+import java.awt.image.BufferedImage;
 
 /**
  * Created by Michael on 04/12/2014.
@@ -10,19 +13,67 @@ import Ludus.Utilitates.Situs;
  */
 public abstract class Res {
 
+    private Simulacrum drawer;
+
     private Situs location;
     private Situs movement;
 
-    protected Res(){
+    protected Res() { init(); }
 
+    protected Res(float xPosition, float yPosition, float xVelocity, float yVelocity, Simulacrum drawer){
+        this(new Situs(xPosition, yPosition), new Situs(xVelocity, yVelocity), drawer);
     }
 
-    public void setLocation(Situs location)   { this.location.set(location); }
-    public void setLocation(float x, float y) { this.location.set(x, y);     }
+    protected Res(Situs location, float xVelocity, float yVelocity, Simulacrum drawer) {
+        this(location, new Situs(xVelocity, yVelocity), drawer);
+    }
 
-    public void setMovement(Situs movement)   { this.movement.set(movement); }
-    public void setMovement(float x, float y) { this.movement.set(x, y);     }
-    public void setMovement(Angulus angle, float velocity) {
+    protected Res(float xPosition, float yPosition, Situs movement, Simulacrum drawer){
+        this(new Situs(xPosition, yPosition), movement, drawer);
+    }
+
+    protected Res(Situs location, Situs movement, Simulacrum drawer) {
+        this();
+        setDrawer(drawer);
+        setLocation(location);
+        setMovement(movement);
+    }
+
+    protected Res(Res copy) {
+        this();
+        set(copy);
+    }
+
+    public final void draw(BufferedImage image) { drawer.draw(image);}
+
+    public final Situs getLocation() { return (new Situs(location)); }
+    public final Situs getMovement() { return (new Situs(movement)); }
+
+    public final float getXLocation() { return (location.getX()); }
+    public final float getYLocation() { return (location.getY()); }
+
+    public final float getXMovement() { return (movement.getX()); }
+    public final float getYMovement() { return (movement.getY()); }
+
+    private void init() {
+        location = new Situs();
+        movement = new Situs();
+    }
+
+    public final void set(Res copy) {
+        setDrawer(copy.drawer);
+        setMovement(copy.movement);
+        setLocation(copy.location);
+    }
+
+    public final void setDrawer(Simulacrum drawer) { this.drawer = drawer; }
+
+    public final void setLocation(Situs location)   { this.location.set(location); }
+    public final void setLocation(float x, float y) { this.location.set(x, y);     }
+
+    public final void setMovement(Situs movement)   { this.movement.set(movement); }
+    public final void setMovement(float x, float y) { this.movement.set(x, y);     }
+    public final void setMovement(Angulus angle, float velocity) {
         this.movement.set(velocity*angle.cos(), velocity*angle.sin());
     }
 
