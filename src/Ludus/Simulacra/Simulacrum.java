0a24d707cdb91c46299dc854b73726b0a3f59eec
Michael van Dyk <msvandyk@hotmail.com>
2014-12-05 19:23:10 -0500

    Added drawing abstract interface, all drawing objects will have to follow this

---
+++ src/Ludus/Simulacra/Simulacrum.java
@@ -0,0 +1,16 @@
+package Ludus.Simulacra;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by Michael on 05/12/2014.
+ *
+ *  Simulacrum interface (drawing(image of) interface)
+ *     All drawing related classes must have the following for drawing
+ *
+ */
+public abstract interface Simulacrum {
+
+    public abstract void draw(BufferedImage image);
+
+}
