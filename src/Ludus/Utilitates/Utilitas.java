927e921a63cf2f47bf18a1634fbdc1c19a9ceb3a
Michael van Dyk <msvandyk@hotmail.com>
2014-12-07 16:20:06 -0500

    Updated to have parameters for each primitive

--- src/Ludus/Utilitates/Utilitas.java
+++ src/Ludus/Utilitates/Utilitas.java
@@ -6,7 +6,18 @@ package Ludus.Utilitates;
  */
 public final class Utilitas {
 
-    public static boolean inRange   (int x, int low, int high) { return (low <= x && high >= x); }
-    public static boolean outOfRange(int x, int low, int high) { return (!inRange(x, low, high)); }
+    public static boolean inRange   (byte x, byte low, byte high)       { return (low <= x && high >= x);  }
+    public static boolean inRange   (double x, double low, double high) { return (low <= x && high >= x);  }
+    public static boolean inRange   (float x, float low, float high)    { return (low <= x && high >= x);  }
+    public static boolean inRange   (int x, int low, int high)          { return (low <= x && high >= x);  }
+    public static boolean inRange   (long x, long low, long high)       { return (low <= x && high >= x);  }
+    public static boolean inRange   (short x, short low, short high)    { return (low <= x && high >= x);  }
+
+    public static boolean outOfRange(byte x, byte low, byte high)       { return (!inRange(x, low, high)); }
+    public static boolean outOfRange(double x, double low, double high) { return (!inRange(x, low, high)); }
+    public static boolean outOfRange(float x, float low, float high)    { return (!inRange(x, low, high)); }
+    public static boolean outOfRange(int x, int low, int high)          { return (!inRange(x, low, high)); }
+    public static boolean outOfRange(long x, long low, long high)       { return (!inRange(x, low, high)); }
+    public static boolean outOfRange(short x, short low, short high)    { return (!inRange(x, low, high)); }
 
 }
