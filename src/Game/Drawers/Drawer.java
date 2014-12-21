6143410cd7869b7fa3028a46ac6e9a07782dfa8c
Michael van Dyk <msvandyk@hotmail.com>
2014-12-21 16:41:00 -0500

    Made generic for the toDraw parameter, is now what object is being drawn.

---
+++ src/Game/Drawers/Drawer.java
@@ -0,0 +1,20 @@
+package Game.Drawers;
+
+import java.awt.*;
+
+/**
+ * Created by Michael on 05/12/2014.
+ *
+ *     The Drawer interface represents the structure that
+ *  something must follow to be draw to screen. Each object
+ *  that gets drawn to screen needs its own separate drawer
+ *  object that implements the drawing
+ *     T - is the class that the Drawer is handling
+ *          e.g. StarDrawer implements Drawer<Star>
+ *
+ */
+public abstract interface Drawer<T> {
+
+    public abstract void draw(T toDraw, Graphics2D g2d);
+
+}
