600dc2ae58d477555098e0195a449a8eb1d96414
Michael van Dyk <msvandyk@hotmail.com>
2014-12-22 01:32:45 -0500

    Added the random functions

--- src/Game/Utilities/Util.java
+++ src/Game/Utilities/Util.java
@@ -10,4 +10,17 @@ public final class Util {
 
     public static boolean outOfRange(double x, double low, double high) { return (!inRange(x, low, high)); }
 
+    public static boolean randBool() { return (1 == (int)(Math.random()*2)); }
+
+    public static int randIntRange(int max)          { return (randIntRange(0, max)); }
+
+    public static int randIntRange(int min, int max) {
+        if (min > max) {
+            int temp = max;
+            max = min;
+            min = temp;
+        }
+        return ((int)(Math.random()*(max-min+1))+min);
+    }
+
 }
