5319d220b7dceaa3ce1b5e0080d300550ddd15e6
Michael van Dyk <msvandyk@hotmail.com>
2014-12-22 01:32:09 -0500

    Added a StaticCollectiveThing, stores a set amount of things

---
+++ src/Game/Things/AreaThings/CollectiveThings/StaticCollectiveThing.java
@@ -0,0 +1,34 @@
+package Game.Things.AreaThings.CollectiveThings;
+
+import Game.Things.AreaThings.AreaThing;
+import Game.Things.Thing;
+import Game.Utilities.XYValue;
+
+
+/**
+ * Created by Michael on 21/12/2014.
+ */
+public abstract class StaticCollectiveThing<E extends Thing> extends AreaThing {
+
+    private E[] contents;
+    private int count;
+
+    @SuppressWarnings("unchecked")
+    public StaticCollectiveThing(XYValue location, XYValue dimensions, int count) {
+        super(location, dimensions);
+        this.count = count;
+        contents = (E[]) (new Thing[this.count]);
+    }
+
+    public E get(int index) { return contents[index]; }
+    public void set(E value, int index) { contents[index] = value; }
+
+    public int getCount() { return (count); }
+
+    @Override
+    public void update() {
+        for (E e : contents)
+            e.update();
+    }
+
+}
