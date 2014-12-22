bfb9f8575c25ae8d01b223463eba73ed90b8c3f8
Michael van Dyk <msvandyk@hotmail.com>
2014-12-22 01:34:01 -0500

    Added the AreaThing, looks over a rectangular area (NOT FOR COLLISION DETECTION)

--- src/Game/Things/AreaThings/AreaThing.java
+++ src/Game/Things/AreaThings/AreaThing.java
@@ -1,9 +1,33 @@
 package Game.Things.AreaThings;
 
 import Game.Things.Thing;
+import Game.Utilities.Util;
+import Game.Utilities.XYValue;
 
 /**
  * Created by Michael on 21/12/2014.
  */
 public abstract class AreaThing extends Thing {
+
+    private XYValue dimensions;
+
+    public AreaThing(XYValue location, XYValue dimensions) {
+        super(location);
+        this.dimensions = new XYValue(dimensions);
+    }
+
+    public XYValue getDimensions() { return (dimensions); }
+
+    public boolean isInside(XYValue point) {
+        return (Util.inRange(point.getX(), getX(), getX() + dimensions.getX()) &&
+                Util.inRange(point.getY(), getY(), getY() + dimensions.getY()));
+    }
+
+    public boolean isOutside(XYValue point) { return (!isInside(point)); }
+
+    public float randomX() { return ((float)(Math.random() * dimensions.getX() + getX())); }
+    public float randomY() { return ((float)(Math.random() * dimensions.getY() + getY())); }
+
+    public XYValue randomLocation() { return (new XYValue(randomX(), randomY())); }
+
 }
